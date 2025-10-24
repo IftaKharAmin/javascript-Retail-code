60f70028c128e79ec7440bee016142417db5e7d8
Shuvo <iftakharulamin01@gmail.com>
2025-10-24 23:45:28 +0600

    laren function

---
+++ condition.js
@@ -0,0 +1,23 @@
+let age = 15;
+// let withadult = true;
+
+
+if( age >= 15) {
+   
+    console.log("apni melai jata parban")
+    // if(withadult) {
+    //     console.log("apni boutik barite jata parban")
+    // }
+
+    // else{
+    //     console.log("apni boutik bari te jaite parban na ");
+
+    // }
+
+}
+
+ 
+
+else{
+    console.log("Apni malai jaite parban na");
+}
---
+++ function.js
@@ -0,0 +1,17 @@
+const person = {
+    name: "shuvo",
+    age:20,
+
+}
+
+const person2 = {
+    Name:"s",
+    age:20
+    
+}
+
+function arrayofObjects(object , object2, x){
+    console.log(object.name , object2.age + x )
+}
+
+arrayofObjects(person, person2,  10);

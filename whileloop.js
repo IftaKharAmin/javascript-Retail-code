68bf0abdb7529ea3b3c9801d0172e30cc7e2dc85
Shuvo <iftakharulamin01@gmail.com>
2025-10-25 10:35:41 +0600

    its a while and do while loop

---
+++ whileloop.js
@@ -0,0 +1,20 @@
+// let i = 0;
+
+// while(i < 10){
+//     console.log("Hello shuvo");
+//     i++
+// }
+
+// let n = 0;
+
+// do{
+//     console.log(n)
+//     n++
+// } while(n < 5)
+
+let x = 5;
+
+do{
+    console.log(x)
+    x++
+} while(x < 4)
